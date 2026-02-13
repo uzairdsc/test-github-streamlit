5a8d76cb54a154a1765a7aed5324d2b210dfd3d9
Uzair Hussain (alt account) <adanchawla44@gmail.com>
2026-02-13 22:23:23 +0500

    Update app-upd.py

--- app-upd.py
+++ app-upd.py
@@ -317,7 +317,8 @@ if st.session_state.df is not None:
     #     type=["xlsx", "csv"],
     #     key="squad_upload"
     # )
-    squad_file = "../data/daily_updated_t20_data/2026-WT20-Squads.xlsx"
+    # squad_file = "../data/daily_updated_t20_data/2026-WT20-Squads.xlsx"
+    squad_file = "data/2026-WT20-Squads.xlsx"
 
     if squad_file:
         # Read squad file
@@ -2014,3 +2015,4 @@ if df is not None:
 
 else:
     st.info("Please select a dataset source to begin.")
+
